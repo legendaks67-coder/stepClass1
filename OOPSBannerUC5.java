67f1a4f23531f8abf2d1756d68d76f0245cbe080
Akshat Maurya <legendaks67@gmail.com>
2026-03-17 08:34:51 +0530

    uc5

---
+++ OOPSBannerUC5.java
@@ -0,0 +1,56 @@
+
+public class OOPSBannerUC5 {
+
+    public static void main(String[] args) {
+
+        String[] banner = {
+            String.join("  ",
+                    " ***** ",
+                    " ***** ",
+                    " ******",
+                    " ***** "
+            ),
+            String.join("  ",
+                    "*     *",
+                    "*     *",
+                    "*     ",
+                    "*     "
+            ),
+            String.join("  ",
+                    "*     *",
+                    "*     *",
+                    "*     ",
+                    "*     "
+            ),
+            String.join("  ",
+                    "*     *",
+                    " ***** ",
+                    " ***** ",
+                    " ***** "
+            ),
+            String.join("  ",
+                    "*     *",
+                    "*     ",
+                    "      *",
+                    "      *"
+            ),
+            String.join("  ",
+                    "*     *",
+                    "*     ",
+                    "      *",
+                    "      *"
+            ),
+            String.join("  ",
+                    " ***** ",
+                    " ***** ",
+                    " ***** ",
+                    " ***** "
+            )
+        };
+
+        // Enhanced for loop
+        for (String line : banner) {
+            System.out.println(line);
+        }
+    }
+}
